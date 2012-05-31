d85fce0f4190a8b21afb0fcee0f0f283473fbde1
Cam Spiers <cameron@heyday.co.nz>
2012-05-31 18:05:29 +1200

    New options for multiple commands

--- code/OptimisedImage.php
+++ code/OptimisedImage.php
@@ -1,174 +1,216 @@
 <?php
 
+/**
+ * 
+ */
 class OptimisedImage extends Image
 {
 
-	protected static $default_quality = 85;
-	protected static $enabled = true;
-	protected static $type_commands = array(
-		2 => 'jpegoptim -p -m$Quality --strip-all $Filename',
-		3 => 'optipng $Filename'
-	);
-	protected static $bin_directory = '';
-	protected static $exec_ending = ' > /dev/null 2>&1 &';
-	protected static $logging = false;
-	protected static $logging_file = '../heyday-optimisedimage/logs/output.log';
-	protected static $background_processing = true;
+    protected static $default_quality = 85;
 
-	protected $quality = false;
+    protected static $enabled = true;
 
-	public static function set_default_quality($qual)
-	{
+    protected static $type_commands = array(
+        2 => array(
+            'jpegoptim' => 'jpegoptim -p -m$Quality --strip-all $Filename'
+        ),
+        3 => array(
+            'optipng' => 'optipng $Filename',
+            'pngcrush' => 'pngcrush -rem gAMA -rem cHRM -rem iCCP -rem sRGB -ow $Filename'
+        )
+    );
 
-		self::$default_quality = $qual;
-		
-	}
+    protected static $enabled_commands = array();
 
-	public static function set_enabled($enabled)
-	{
+    protected static $bin_directory = '';
 
-		self::$enabled = $enabled;
-		
-	}
+    protected static $exec_ending = ' > /dev/null 2>&1 &';
 
-	public static function add_type_command($image_type, $cmd)
-	{
-		
-		switch (strtolower($image_type)) {
+    protected static $logging = false;
 
-			case 'gif':
-				self::$type_commands[1] = $cmd;
-				break;
+    protected static $logging_file = '../heyday-optimisedimage/logs/output.log';
 
-			case 'jpg':
-			case 'jpeg':
-				self::$type_commands[2] = $cmd;
-				break;
+    protected static $background_processing = true;
 
-			case 'png':
-				self::$type_commands[3] = $cmd;
-				break;
+    protected $quality = false;
 
-		}
+    public static function set_default_quality($qual)
+    {
 
-	}
+        self::$default_quality = $qual;
+        
+    }
 
-	public static function set_bin_directory($directory)
-	{
+    public static function set_enabled($enabled)
+    {
 
-		self::$bin_directory = $directory;
-		
-	}
+        self::$enabled = $enabled;
 
-	public static function set_exec_ending($ending)
-	{
+    }
 
-		self::$exec_ending = $ending;
-		
-	}
+    public static function set_enabled_commands($enabled_commands)
+    {
 
-	public static function set_logging($logging)
-	{
+        foreach ($enabled_commands as $type => $bin_name) {
 
-		self::$logging = $logging;
-		
-	}
+            if (is_int($type)) {
 
-	public static function set_logging_file($file)
-	{
+                self::$enabled_commands[$type] = $bin_name;
 
-		self::$logging_file = $file;
-		
-	}
+            } else {
 
-	public static function set_background_processing($enabled)
-	{
+                self::$enabled_commands[self::get_number_by_type($type)] = $bin_name;
 
-		self::$background_processing = $enabled;
-		
-	}
+            }
 
-	public function setQuality($qual)
-	{
+        }
 
-		$this->quality = $qual;
-		
-	}
+    }
 
-	public function getQuality()
-	{
+    public static function get_number_by_type($imageType)
+    {
 
-		return $this->quality ? $this->quality : self::$default_quality;
+        switch (strtolower($imageType)) {
 
-	}
-	
-	function generateFormattedImage($format, $arg1 = null, $arg2 = null) {
+        case 'gif':
+            return 1;
 
-		$cacheFile = $this->cacheFilename($format, $arg1, $arg2);
-	
-		$gd = new GD(Director::baseFolder()."/" . $this->Filename);
-		
-		
-		if($gd->hasGD()){
+        case 'jpg':
+        case 'jpeg':
+            return 2;
 
-			$generateFunc = "generate$format";
+        case 'png':
+            return 3;
 
-			if($this->hasMethod($generateFunc)){
+        }
 
-				$gd = $this->$generateFunc($gd, $arg1, $arg2);
+        return 0;
 
-				$resampledFile = Director::baseFolder() . '/' . $cacheFile;
+    }
 
-				if($gd){
+    public static function add_type_command($imageType, $bin_name, $cmd)
+    {
 
-					$gd->writeTo($resampledFile);
+        self::$type_commands[self::get_number_by_type($imageType)][$bin_name] = $cmd;
 
-				}
+    }
 
-				if (self::$enabled) {
+    public static function set_bin_directory($directory)
+    {
 
-					list($width, $height, $type, $attr) = getimagesize($resampledFile);
+        self::$bin_directory = $directory;
 
-					if (isset(self::$type_commands[$type])) {
+    }
 
-						$viewer = new SSViewer_FromString(self::$type_commands[$type]);
+    public static function set_exec_ending($ending)
+    {
 
-						if (self::$logging) {
+        self::$exec_ending = $ending;
 
-							$output = array();
+    }
 
-							exec(self::$bin_directory . $viewer->process(new ArrayData(array(
-								'Quality' => $this->getQuality(),
-								'Filename' => $resampledFile
-							))), $output);
+    public static function set_logging($logging)
+    {
 
-							if (count($output)) {
+        self::$logging = $logging;
 
-								file_put_contents(self::$logging_file, implode(PHP_EOL, $output) . PHP_EOL, FILE_APPEND);
-								
-							}
-							
-						} else {
+    }
 
-							exec(self::$bin_directory . $viewer->process(new ArrayData(array(
-								'Quality' => $this->getQuality(),
-								'Filename' => $resampledFile
-							))) . (self::$background_processing ? self::$exec_ending : ''));
-							
-						}
+    public static function set_logging_file($file)
+    {
 
-					}
+        self::$logging_file = $file;
 
-				}
-	
-			} else {
+    }
 
-				USER_ERROR("Image::generateFormattedImage - Image $format function not found.",E_USER_WARNING);
+    public static function set_background_processing($enabled)
+    {
 
-			}
+        self::$background_processing = $enabled;
 
-		}
+    }
 
-	}
-	
+    public function setQuality($qual)
+    {
+
+        $this->quality = $qual;
+
+    }
+
+    public function getQuality()
+    {
+
+        return $this->quality ? $this->quality : self::$default_quality;
+
+    }
+
+    function generateFormattedImage($format, $arg1 = null, $arg2 = null) {
+
+        $cacheFile = $this->cacheFilename($format, $arg1, $arg2);
+
+        $gd = new GD(Director::baseFolder()."/" . $this->Filename);
+
+
+        if($gd->hasGD()){
+
+            $generateFunc = "generate$format";
+
+            if($this->hasMethod($generateFunc)){
+
+                $gd = $this->$generateFunc($gd, $arg1, $arg2);
+
+                $resampledFile = Director::baseFolder() . '/' . $cacheFile;
+
+                if($gd){
+
+                    $gd->writeTo($resampledFile);
+
+                }
+
+                if (self::$enabled) {
+
+                    list($width, $height, $type, $attr) = getimagesize($resampledFile);
+
+                    if (isset(self::$enabled_commands[$type]) && isset(self::$type_commands[$type][self::$enabled_commands[$type]])) {
+
+                        $viewer = new SSViewer_FromString(self::$type_commands[$type][self::$enabled_commands[$type]]);
+
+                        if (self::$logging) {
+
+                            $output = array();
+
+                            exec(self::$bin_directory . $viewer->process(new ArrayData(array(
+                                'Quality' => $this->getQuality(),
+                                'Filename' => $resampledFile
+                            ))), $output);
+
+                            if (count($output)) {
+
+                                file_put_contents(self::$logging_file, implode(PHP_EOL, $output) . PHP_EOL, FILE_APPEND);
+                                
+                            }
+                            
+                        } else {
+
+                            exec(self::$bin_directory . $viewer->process(new ArrayData(array(
+                                'Quality' => $this->getQuality(),
+                                'Filename' => $resampledFile
+                            ))) . (self::$background_processing ? self::$exec_ending : ''));
+                            
+                        }
+
+                    }
+
+                }
+    
+            } else {
+
+                user_error("Image::generateFormattedImage - Image $format function not found.", E_USER_WARNING);
+
+            }
+
+        }
+
+    }
+    
 }
